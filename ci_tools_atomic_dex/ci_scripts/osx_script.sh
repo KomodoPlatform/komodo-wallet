f275ebb34f421eebf55cb772f219320822bdf826
smk762 <35845239+smk762@users.noreply.github.com>
2022-03-23 22:41:22 +0800

    Create osx_script.sh

--- ci_tools_atomic_dex/ci_scripts/osx_script.sh
+++ ci_tools_atomic_dex/ci_scripts/osx_script.sh
@@ -5,6 +5,9 @@ brew update
 brew tap-new $USER/local-nim
 brew extract --version=1.4.8 nim $USER/local-nim
 
+brew tap-new $USER/local-libtool
+brew extract --version=2.4.6 libtool $USER/local-libtool
+
 
 brew install autoconf \
             automake \
@@ -12,7 +15,7 @@ brew install autoconf \
             wget \
             nim@1.4.8 \
             ninja \
-            libtool \
+            libtool@2.4.6 \
             gnu-sed \
             coreutils \
             llvm \
@@ -34,7 +37,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.15
 #make -j3 install
 #cd ../
 
-git clone https://github.com/KomodoPlatform/libwally-core.git
+git clone https://github.com/ElementsProject/libwally-core.git
 cd libwally-core
 ./tools/autogen.sh
 ./configure --disable-shared
