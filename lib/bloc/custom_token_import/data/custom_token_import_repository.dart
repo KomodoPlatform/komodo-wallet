80476e732346b7ab5ba87f7e5c33dd1f94254594
naezith <naezith@gmail.com>
2024-11-20 18:37:30 +0300

    custom token import KdfCustomTokenImportRepository

--- lib/bloc/custom_token_import/data/custom_token_import_repository.dart
+++ lib/bloc/custom_token_import/data/custom_token_import_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:web_dex/bloc/coins_bloc/coins_repo.dart';
 import 'package:web_dex/model/coin_type.dart';
 
 abstract class ICustomTokenImportRepository {
@@ -23,6 +24,38 @@ class CustomTokenImportMockRepository implements ICustomTokenImportRepository {
     return {
       "abbr": "BTC",
       "image_url": 'assets/coin_icons/png/btc.png',
+      "balance": '50',
+      "usd_balance": '200',
+    };
+  }
+
+  @override
+  Future<void> importCustomToken(
+      CoinType network, String address, int decimals) async {
+    await Future.delayed(const Duration(seconds: 2));
+  }
+}
+
+class KdfCustomTokenImportRepository implements ICustomTokenImportRepository {
+  @override
+  Future<Map<String, dynamic>> fetchCustomToken(
+      CoinType network, String address, int decimals) async {
+    final response = await coinsRepo.getTokenInfo(network, address, decimals);
+    final tokenInfo = response?['result'];
+
+    if (tokenInfo == null) {
+      throw 'Failed to get token info';
+    }
+
+    String abbr = tokenInfo['config_ticker'] ?? tokenInfo['info']['symbol'];
+
+    return {
+      "abbr": abbr,
+      "decimals": tokenInfo['info']['decimals'],
+      // TODO: Acquire image url from a web API
+      "image_url": 'assets/coin_icons/png/${abbr.toLowerCase()}.png',
+
+      // TODO: Get balance by temporarily activating the coin
       "balance": '50',
       "usd_balance": '200',
     };
