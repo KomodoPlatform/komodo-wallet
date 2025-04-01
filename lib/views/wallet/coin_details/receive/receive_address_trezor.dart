82f85190a29b21a1abf888817011e6ccf5fe0934
Francois <takenagain@users.noreply.github.com>
2025-04-01 21:19:09 +0200

    fix(withdraw): evm address conversion format error

--- lib/views/wallet/coin_details/receive/receive_address_trezor.dart
+++ lib/views/wallet/coin_details/receive/receive_address_trezor.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:komodo_defi_types/komodo_defi_types.dart';
 import 'package:komodo_ui/komodo_ui.dart';
 import 'package:komodo_ui_kit/komodo_ui_kit.dart';
-import 'package:web_dex/bloc/coins_bloc/asset_coin_extension.dart';
 import 'package:web_dex/generated/codegen_loader.g.dart';
 import 'package:web_dex/shared/utils/utils.dart';
 import 'package:web_dex/views/wallet/coin_details/receive/request_address_button.dart';
