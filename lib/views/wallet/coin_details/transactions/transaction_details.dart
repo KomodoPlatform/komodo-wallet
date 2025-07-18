465d4ce562314a3e5cf1827b45b2cb677aaffab3
Charl (Nitride) <77973576+CharlVS@users.noreply.github.com>
2025-07-18 14:17:37 +0200

    feat: display complete addresses in transaction details

--- lib/views/wallet/coin_details/transactions/transaction_details.dart
+++ lib/views/wallet/coin_details/transactions/transaction_details.dart
@@ -142,11 +142,10 @@ class TransactionDetails extends StatelessWidget {
                 Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 14),
           ),
           const SizedBox(width: 8),
-          ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 200),
+          Flexible(
             child: CopiedText(
               copiedValue: address,
-              isTruncated: true,
+              isTruncated: false,
               padding: const EdgeInsets.symmetric(
                 vertical: 8,
                 horizontal: 16,
