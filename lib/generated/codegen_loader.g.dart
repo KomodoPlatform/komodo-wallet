12e905be72572e5c57e345f9a3236aed43bdab75
dragonhound <35845239+smk762@users.noreply.github.com>
2025-07-22 17:58:52 +0800

    q pdate "seed" to "seed phrase" in login/import forms (#2972)
    * fix translation string name
    
    * `seed` - > `seed phrase`
    
    * removes "seed" in connect button

--- lib/generated/codegen_loader.g.dart
+++ lib/generated/codegen_loader.g.dart
@@ -350,7 +350,6 @@ abstract class LocaleKeys {
   static const exchange = 'exchange';
   static const connectSomething = 'connectSomething';
   static const hardwareWallet = 'hardwareWallet';
-  static const komodoWalletSeed = 'komodoWalletSeed';
   static const metamask = 'metamask';
   static const comingSoon = 'comingSoon';
   static const walletsTypeListTitle = 'walletsTypeListTitle';
@@ -537,7 +536,7 @@ abstract class LocaleKeys {
       'enterPassphraseHiddenWalletDescription';
   static const skip = 'skip';
   static const activateToSeeFunds = 'activateToSeeFunds';
-  static const allowCustomFee = 'allowCustomFee';
+  static const useCustomSeedOrWif = 'useCustomSeedOrWif';
   static const cancelOrder = 'cancelOrder';
   static const version = 'version';
   static const copyAddressToClipboard = 'copyAddressToClipboard';
