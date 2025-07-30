bacd094f0ebda8a3914d6d57fbcdca2139ca4598
CharlVS <77973576+CharlVS@users.noreply.github.com>
2025-07-30 21:22:42 +0200

    chore: delete unused types
    Delete HD-related types. Previous references have likely been migrated to the SDK types.
    
    TODO: Investigate if there are any other similar cases.

--- lib/views/wallet/coin_details/coin_details_info/charts/coin_sparkline.dart
+++ lib/views/wallet/coin_details/coin_details_info/charts/coin_sparkline.dart
@@ -2,7 +2,6 @@ import 'package:dragon_charts_flutter/dragon_charts_flutter.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:komodo_cex_market_data/komodo_cex_market_data.dart';
-import 'package:komodo_ui_kit/komodo_ui_kit.dart';
 import 'package:web_dex/generated/codegen_loader.g.dart';
 import 'package:web_dex/shared/utils/utils.dart';
 
